cf9ed371dd12cc090d31593b60abd5f9b4c3531b
ttu <tomi.tuhkanen@iki.fi>
2017-07-01 00:44:06 +0300

    Add itemId to websocket message on post

--- FakeServer/WebSockets/NotifyWebSocketMiddlerware.cs
+++ FakeServer/WebSockets/NotifyWebSocketMiddlerware.cs
@@ -25,7 +25,17 @@ namespace FakeServer.WebSockets
                 _udpateMethods.Contains(context.Request.Method) &&
                 (context.Response.StatusCode >= 200 && context.Response.StatusCode < 300))
             {
-                var data = ObjectHelper.GetWebSocketMessage(context.Request.Method, context.Request.Path.Value);
+                var method = context.Request.Method;
+                var path = context.Request.Path.Value;
+
+                if (method == "POST")
+                {
+                    var location = context.Response.Headers["Location"].ToString();
+                    var itemId = location.Substring(location.LastIndexOf('/') + 1);
+                    path = $"{path}/{itemId}";
+                }
+
+                var data = ObjectHelper.GetWebSocketMessage(method, path);
                 _bus.Publish("updated", data);
             }
         }
