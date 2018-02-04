82fbdaecfb013f70218fca2d98b674f72ea26f79
erichoog <eric_hoog@hotmail.com>
2018-02-04 01:11:55 -0500

    add SortHelper

---
+++ FakeServer/Common/SortHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+
+namespace FakeServer.Common
+{
+    public static class SortHelper
+    {
+        public static IEnumerable<dynamic> SortFields(IEnumerable<dynamic> results, IEnumerable<string> sortFields)
+        {
+            IOrderedEnumerable<dynamic> sortedResults = null;
+            int i = 0;
+
+            sortFields.ToList().ForEach(x => 
+            {
+                if (i == 0)
+                    sortedResults = GetFirstSortResults(results, x, IsSortDescending(x));
+                else
+                    sortedResults = GetRemainingSortResults(sortedResults, x, IsSortDescending(x));
+
+            });
+
+            return sortedResults.AsEnumerable();
+        }
+
+        private static IOrderedEnumerable<dynamic> GetFirstSortResults(IEnumerable<dynamic> results, string sortField, bool isSortDescending)
+        {
+            sortField = RemoveSortDirection(sortField);
+            IOrderedEnumerable<dynamic> sortResults;
+
+            if(isSortDescending)
+                sortResults = results.OrderByDescending(x => ObjectHelper.GetNestedProperty((x as ExpandoObject), sortField));
+            else
+                sortResults = results.OrderBy(x => ObjectHelper.GetNestedProperty((x as ExpandoObject), sortField));
+
+            return sortResults;
+        }
+
+        private static IOrderedEnumerable<dynamic> GetRemainingSortResults(IOrderedEnumerable<dynamic> results, string sortField, bool isSortDescending)
+        {
+            sortField = RemoveSortDirection(sortField);
+            IOrderedEnumerable<dynamic> sortResults;
+
+            if(isSortDescending)
+                sortResults = results.ThenByDescending(x => ObjectHelper.GetNestedProperty((x as ExpandoObject), sortField));
+            else
+                sortResults = results.ThenBy(x => ObjectHelper.GetNestedProperty((x as ExpandoObject), sortField));
+
+            return sortResults;
+        }
+
+        private static bool IsSortDescending(string sortField)
+        {
+            if (sortField.Contains("+")) return false;
+            else return true;
+        }
+
+        private static string RemoveSortDirection(string sortField)
+        {
+            return sortField.Replace("+", "").Replace("-","");
+        }
+    }
+}
