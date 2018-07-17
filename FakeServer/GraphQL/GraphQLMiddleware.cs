223627e2efb9e7572f77cb9756ff797478593934
Antonia Elek <antoniaelek@hotmail.com>
2018-07-17 08:10:31 +0200

    Implement POST with application/json in GraphQL Middleware. (#24)

--- FakeServer/GraphQL/GraphQLMiddleware.cs
+++ FakeServer/GraphQL/GraphQLMiddleware.cs
@@ -3,6 +3,8 @@ using FakeServer.WebSockets;
 using JsonFlatFileDataStore;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -16,6 +18,7 @@ namespace FakeServer.GraphQL
         private readonly IDataStore _datastore;
         private readonly IMessageBus _bus;
         private readonly bool _authenticationEnabled;
+        private readonly string[] _allowedTypes = new[] { "application/graphql", "application/json" };
 
         public GraphQLMiddleware(RequestDelegate next, IDataStore datastore, IMessageBus bus, bool authenticationEnabled)
         {
@@ -28,7 +31,7 @@ namespace FakeServer.GraphQL
         public async Task Invoke(HttpContext context)
         {
             // POST application/graphql body is query
-            // TODO: POST application/json and { "query": "..." }
+            // POST application/json and { "query": "..." }
             // TODO: POST /graphql?query={users{name}}
             // TODO: GET /graphql?query={users{name}}
 
@@ -44,25 +47,29 @@ namespace FakeServer.GraphQL
                 return;
             }
 
-            if (context.Request.Method != "POST" || !context.Request.ContentType.Contains("application/graphql"))
+            if (context.Request.Method != "POST" || !_allowedTypes.Any(context.Request.ContentType.Contains))
             {
                 context.Response.StatusCode = (int)HttpStatusCode.NotImplemented;
                 await context.Response.WriteAsync(JsonConvert.SerializeObject(new { errors = new[] { "Not implemented" } }));
                 return;
             }
 
-            var query = string.Empty;
+            GraphQLResult result = null;
 
-            using (var streamReader = new StreamReader(context.Request.Body))
+            var (success, query, error) = await ParseQuery(context);
+
+            if (!success)
             {
-                query = await streamReader.ReadToEndAsync().ConfigureAwait(true);
+                result = new GraphQLResult { Errors = new List<string> { error } };
             }
+            else
+            {
+                var toReplace = new[] { "\r\n", "\\r\\n", "\\n", "\n" };
 
-            var toReplace = new[] { "\r\n", "\\r\\n", "\\n", "\n" };
+                query = toReplace.Aggregate(query, (acc, curr) => acc.Replace(curr, ""));
 
-            query = toReplace.Aggregate(query, (acc, curr) => acc.Replace(curr, ""));
-
-            var result = await GraphQL.HandleQuery(query, _datastore);
+                result = await GraphQL.HandleQuery(query, _datastore);
+            }
 
             var json = result.Errors?.Any() == true
                             ? JsonConvert.SerializeObject(new { data = result.Data, errors = result.Errors })
@@ -74,6 +81,39 @@ namespace FakeServer.GraphQL
             context.Response.StatusCode = result.Errors?.Any() == true ? (int)HttpStatusCode.BadRequest : (int)HttpStatusCode.OK;
 
             await context.Response.WriteAsync(json);
+        }
+
+        private static async Task<(bool success, string body, string error)> ParseQuery(HttpContext context)
+        {
+            string body;
+
+            using (var streamReader = new StreamReader(context.Request.Body))
+            {
+                body = await streamReader.ReadToEndAsync().ConfigureAwait(true);
+            }
+
+            if (context.Request.ContentType.StartsWith("application/graphql"))
+            {
+                return (true, body, null);
+            }
+
+            dynamic jsonBody;
+
+            try
+            {
+                jsonBody = JsonConvert.DeserializeObject(body);
+            }
+            catch (Exception e)
+            {
+                return (false, null, e.Message);
+            }
+
+            if (jsonBody.query is null)
+            {
+                return (false, null, "Missing query property in json.");
+            }    
+
+            return (true, jsonBody.query, null);
         }
     }
 }
