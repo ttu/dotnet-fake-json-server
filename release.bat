275bf2767025ef7c7b9172e7f058274d506a1836
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2021-03-14 11:12:40 +0200

    Upgrade project to .NET 5 (#79)

--- release.bat
+++ release.bat
@@ -12,11 +12,11 @@ set unixOS=osx-x64 linux-x64
 for %%N in (%winOS%) do (
 	set rid=%%N
 	dotnet publish ./FakeServer/FakeServer.csproj -c release -r !rid! /p:PackAsTool=false /p:PublishTrimmed=true
-	7z a -tzip ./releases/fakeserver-!rid!.zip ./FakeServer/bin/release/netcoreapp3.1/!rid!/publish/* -r
+	7z a -tzip ./releases/fakeserver-!rid!.zip ./FakeServer/bin/release/net5.0/!rid!/publish/* -r
 )
 
 for %%N in (%unixOS%) do (
 	set rid=%%N
 	dotnet publish ./FakeServer/FakeServer.csproj -c release -r !rid! /p:PackAsTool=false /p:PublishTrimmed=true
-	7z a -ttar -so ./releases/fakeserver-!rid!.tar ./FakeServer/bin/release/netcoreapp3.1/!rid!/publish/* -r | 7z a -si ./releases/fakeserver-!rid!.tar.gz
+	7z a -ttar -so ./releases/fakeserver-!rid!.tar ./FakeServer/bin/release/net5.0/!rid!/publish/* -r | 7z a -si ./releases/fakeserver-!rid!.tar.gz
 )
