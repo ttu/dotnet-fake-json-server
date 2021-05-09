588d7acb0369e27c0ffa8b2bc20aedd644bd15b2
Lucarine <llliangwenjun193@gmail.com>
2021-05-10 00:29:21 +1000

    implement token swagger filter

--- FakeServer/Authentication/SwaggerFilters.cs
+++ FakeServer/Authentication/SwaggerFilters.cs
@@ -27,7 +27,7 @@ namespace FakeServer.Authentication
     {
         public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
         {
-            var tokenItem =  new OpenApiPathItem();
+            var tokenItem = new OpenApiPathItem();
             tokenItem.AddOperation(OperationType.Post, new OpenApiOperation
             {
                 Tags = new List<OpenApiTag> { new OpenApiTag { Name = "Authentication" } },
@@ -88,6 +88,30 @@ namespace FakeServer.Authentication
             //        }
             //    }
             //});
+        }
+    }
+    
+    internal class TokenOperation : IDocumentFilter
+    {
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+        {
+            var logoutItem = new OpenApiPathItem();
+            logoutItem.AddOperation(OperationType.Post, new OpenApiOperation
+            {
+                Tags = new List<OpenApiTag> { new OpenApiTag { Name = "Authentication" } },
+                Parameters = new List<OpenApiParameter>
+                    {
+                        new OpenApiParameter
+                        {
+                            //Type = "string",
+                            Name = "Authorization",
+                            Required = false,
+                            In = ParameterLocation.Header
+                        }
+                    }
+            });
+
+            swaggerDoc.Paths.Add($"/{Config.TokenLogoutRoute}", logoutItem);
         }
     }
 }
